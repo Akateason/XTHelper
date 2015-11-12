3988e889e6671387d95dfcb18527623388733980
akateason <xietianchen@subaojaojiang.com>
2015-11-12 14:54:50 +0800

    Initial Commit

---
+++ XTHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XTHelper
+//
+//  Created by TuTu on 15/11/12.
+//  Copyright © 2015年 teason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
